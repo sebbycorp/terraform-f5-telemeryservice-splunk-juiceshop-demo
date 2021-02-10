69ccbfcbd46ccece02c2571888e52f6b05dd3f2f
Sebastian Maniak <sebastian@maniak.io>
2021-02-10 12:06:49 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -4,8 +4,64 @@ terraform {
       source = "kreuzwerker/docker"
       version = "2.11.0"
     }
+    bigip = {
+      source = "F5Networks/bigip"
+      version = "1.6.0"
+    }
   }
 }
+
+variable "bigip_password" {
+  type = string
+}
+
+variable "bigip_address" {
+  type = string
+}
+
+provider "bigip" {
+  address  = var.bigip_address
+  username = "admin"
+  password = var.bigip_password
+}
+
+## This example creates a self-signed certificate for a development
+
+resource "tls_private_key" "example" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "tls_self_signed_cert" "example" {
+  key_algorithm   = tls_private_key.example.algorithm
+  private_key_pem = tls_private_key.example.private_key_pem
+
+
+  # Certificate expires after 12 hours.
+  validity_period_hours = 12
+
+  # Generate a new certificate if Terraform is run within three
+  # hours of the certificate's expiration time.
+  early_renewal_hours = 3
+
+  # Reasonable set of uses for a server SSL certificate.
+  allowed_uses = [
+      "key_encipherment",
+      "digital_signature",
+      "server_auth",
+  ]
+
+  dns_names = ["maniak.academy", "maniak.io"]
+
+  subject {
+      common_name  = "maniak.academy"
+      organization = "Maniak Academy, Inc"
+  }
+}
+
+resource "bigip_as3"  "as3app1" {
+       as3_json = file("as3app1.json")
+ }
 
 provider "docker" {
   host = "tcp://localhost:2375/"
@@ -28,6 +84,21 @@ resource "docker_container" "splunk" {
    ports {
     internal = 8088
     external = 8088
+  }
+}
+
+resource "docker_image" "juice-shop" {
+  name = "bkimminich/juice-shop:latest"
+}
+
+
+# Create juicebox container
+resource "docker_container" "juice-shop" {
+  image = docker_image.juice-shop.latest
+  name  = "juice-shop"
+  ports {
+    internal = 3000
+    external = 3001
   }
 }
 
